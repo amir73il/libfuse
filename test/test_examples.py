17cddbfe607d37228a1906030f2ca786295bbda5
Amir Goldstein <amir73il@gmail.com>
2023-09-13 17:31:41 +0300

    Add passthtough_hp test runs with debug logs

--- test/test_examples.py
+++ test/test_examples.py
@@ -191,7 +191,8 @@ def test_passthrough(short_tmpdir, name, debug, output_checker, writeback):
         umount(mount_process, mnt_dir)
 
 @pytest.mark.parametrize("cache", (False, True))
-def test_passthrough_hp(short_tmpdir, cache, output_checker):
+@pytest.mark.parametrize("debug", (False, True))
+def test_passthrough_hp(short_tmpdir, cache, debug, output_checker):
     mnt_dir = str(short_tmpdir.mkdir('mnt'))
     src_dir = str(short_tmpdir.mkdir('src'))
 
@@ -203,7 +204,10 @@ def test_passthrough_hp(short_tmpdir, cache, output_checker):
 
     if not cache:
         cmdline.append('--nocache')
-        
+
+    if debug:
+        cmdline.append('--debug')
+
     mount_process = subprocess.Popen(cmdline, stdout=output_checker.fd,
                                      stderr=output_checker.fd)
     try:
