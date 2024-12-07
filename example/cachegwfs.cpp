067a4c97c361a0b13674008275f144f94236c973
Amir Goldstein <amir73il@gmail.com>
2024-12-07 14:47:32 +0100

    cachegwfs: store folder id in each inode
    First level subdir gets its folder id from its name.
    If first level subdir is not a number, its ancestors have no folder id.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- example/cachegwfs.cpp
+++ example/cachegwfs.cpp
@@ -306,6 +306,11 @@ static fuse_path_at get_fd_path_op(const fuse_path_at &in, enum op op)
 	}
 }
 
+static uint64_t *get_folder_id(const fuse_state_t &state)
+{
+	return (uint64_t *)state.get();
+}
+
 static enum op redirect_open_op(fuse_file_info *fi)
 {
 	enum op op;
@@ -465,7 +470,45 @@ static int cgwfs_lookup(const fuse_path_at &at, fuse_entry_param *e)
 	}
 	// Lookup itself is never in the redirected path, because we
 	// need to find the real xfs inode
-	return next_op(lookup)(at, e);
+	auto ret = next_op(lookup)(at, e);
+	if (ret)
+		return ret;
+
+	// If subdir name is not a decimal number, the folder id is undefined.
+	// For all other inodes, it is inheritted from the parent.
+	fuse_state_t state;
+	fuse_state_t& parent_state = at.inode().get_state(fs);
+	if (at.inode().is_root() && S_ISDIR(e->attr.st_mode)) {
+		uint64_t folder_id = strtoull(at.path(), NULL, 10);
+		if (fs.debug() && folder_id)
+			cerr << "DEBUG: first level subdir folder id "
+				<< folder_id << endl;
+
+		if (folder_id) {
+			auto p = new (nothrow) uint64_t;
+			if (p) {
+				*p = folder_id;
+				state.reset(p);
+			} else if (fs.debug()) {
+				cerr << "ERROR: Allocate inode state failed."
+					<< endl;
+			}
+		}
+	} else if (parent_state) {
+		state = parent_state;
+		if (fs.debug())
+			cerr << "DEBUG: inherit parent folder id "
+				<< *get_folder_id(state) << endl;
+	}
+
+	if (state && !set_module_inode_state(fs, e->ino, state)) {
+		if (fs.debug())
+			cerr << "ERROR: failed setting folder id "
+				<< *get_folder_id(state)
+				<< " ino=" << e->ino << endl;
+	}
+
+	return 0;
 }
 
 static int cgwfs_getattr(const fuse_path_at &in, struct stat *attr,
