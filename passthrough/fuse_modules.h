bdd5b4c7c8964b6f739d71b5e69ded6dfa7ca499
Amir Goldstein <amir73il@gmail.com>
2024-12-08 15:23:56 +0100

    passthrough: create fuse passthrough library
    libfuse_passthrough mirrors a specified "source" directory with as much
    fidelity and performance as possible, allowing to register an optional
    passthrough module.
    
    The passthrough module operations are called as hooks for every
    operation before the default fs operations on the "source" directory
    and call the default fs operations.
    
    The lookup() operation hook gives modules a chance to manifest files in
    the source directory before actual lookup().
    
    The fuse_path_at path argument passed to operations from the fuse
    passthrough library contains, in addition to path information, a
    reference to the low level inode and fuse request.
    
    The passthrough_fs.cc example implements a trivial passthrough
    filesystem, which uses the example tracei.cpp passthrough module to
    print debug logs from information found in the fuse_path_at object.
    
    When executed without --debug option, the example demonstrates the
    use of fuse passthrough library without any passthrough module.
    
    When the mirrored "source" filesystem is xfs or ext4, the passthrough
    implementation uses persistent file handles and open_by_handle_at()
    instead of keeping open fd per inode and without the need to pin the
    source inodes to cache while fuse inodes are in inode cache.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

---
+++ passthrough/fuse_modules.h
@@ -0,0 +1,15 @@
+/*
+  FUSE passthrough: FUSE passthrough example modules
+
+  Copyright (C) 2021-2024  CTERA Networks
+
+  This program can be distributed under the terms of the GNU LGPLv2.
+  See the file COPYING.LIB
+*/
+
+#ifndef FUSE_MODULES_H_
+#define FUSE_MODULES_H_
+
+fuse_passthrough_module *trace_module(void);
+
+#endif /* FUSE_MODULES_H_ */
