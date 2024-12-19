faff0a58ed965d950b2fca74e537b25835679ba0
Amir Goldstein <amir73il@gmail.com>
2024-12-19 17:11:14 +0100

    notifyfs: do not index directories newer than index
    If filesytem supports birth time (e.g. xfs, ext4), check btime and
    avoid indexing directories that were created after the index directory.
    
    With this change, the test test_passthrough_hp with notifyfs does
    not index any directory when run on a filesystem that supports birth
    time, because all the directories created in the test are newer than
    the index directory.
    
    Add the --index_all command line option and use it in tests to index
    also new directories in all redirect runs for better test coverage.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- test/test_examples.py
+++ test/test_examples.py
@@ -241,6 +241,10 @@ def test_passthrough_hp(short_tmpdir, mode, name, output_checker):
     if name == 'notifyfs':
         cmdline.append('--index_path=' + index_dir)
         cmdline.append('--debug')
+        # All directories in the test are newer then index, so index also
+        # new directories in all redirect runs for better test coverage
+        if redirect:
+            cmdline.append('--index_all')
 
     cmdline.append('--foreground')
 
