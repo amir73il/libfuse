9e76d479a53ba87ae10f39dc5428b3ee309515f3
Amir Goldstein <amir73il@gmail.com>
2026-05-08 23:31:30 +0200

    passthrough: use kernel passthrough mode for readdir operation
    This is an experimental code to test kernel readdir passthrough.
    kernel readdir passthrough is not yet supported by upstream kernel
    so it is disabled by default.  When enabled, it will disable both
    readdir cache and readdirplus.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- example/passthrough_fs.cc
+++ example/passthrough_fs.cc
@@ -76,6 +76,7 @@ static cxxopts::ParseResult parse_options(int &argc, char **argv, fuse_passthrou
 		("wbcache", "Enable writeback cache")
 		("nosplice", "Do not use splice(2) to transfer data")
 		("nopassthrough", "Do not use kernel pass-through mode for read/write")
+		("readdirpassthrough", "Use kernel pass-through mode for readdir")
 		("single", "Run single-threaded");
 
 	// FIXME: Find a better way to limit the try clause to just
@@ -104,6 +105,7 @@ static cxxopts::ParseResult parse_options(int &argc, char **argv, fuse_passthrou
 	opts.nocache = options.count("nocache");
 	opts.wbcache = !opts.nocache && options.count("wbcache");
 	opts.kernel_passthrough = !options.count("nopassthrough");
+	opts.readdir_passthrough = options.count("readdirpassthrough");
 	opts.attr_timeout = opts.entry_timeout = opts.nocache ? 0 : 1.0;
 	// With --nocache also do not allow keeping open fds
 	if (options.count("nocache")) {
