81c4614146d821735efccd9b6c66c56d012d92ad
Amir Goldstein <amir73il@gmail.com>
2026-09-19 16:58:14 +0200

    passthrough: perform all operations with original user credentials
    Introduce fuse_path_at::with_cred() so default operations that need
    POSIX ACL permission checking (especially do_open and do_lookup) run
    as the requestor. Move Cred scope from pfs_setattr / setxattr into
    the default do_* implementations so modules can stay privileged.
    
    If the filesystem lets the kernel handle POSIX ACLs (def_posixacl),
    skip with_cred() except for operations that change access permissions
    (chmod/chown/setxattr/removexattr) and utimens, which use
    with_cred_force(). Creates still go through as_user().
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- passthrough/fuse_passthrough.cpp
+++ passthrough/fuse_passthrough.cpp
@@ -435,34 +435,10 @@ bool Fs::encode(xfs_fh &xfh)
 #define NULL_UID static_cast<uid_t>(-1)
 #define NULL_GID static_cast<gid_t>(-1)
 
-struct Cred {
-	uid_t _uid {NULL_UID};
-	gid_t _gid {NULL_GID};
+uid_t fuse_path_at::daemon_uid() const { return fs.uid; }
+gid_t fuse_path_at::daemon_gid() const { return fs.gid; }
 
-	Cred() = delete;
-	Cred(const Cred&) = delete;
-	Cred(Cred&&) = delete;
-	Cred& operator=(Cred&&) = delete;
-	Cred& operator=(const Cred&) = delete;
-
-	Cred(uid_t uid, gid_t gid) {
-		// Set requestor credentials
-		if (uid != fs.uid)
-			_uid = setfsuid(uid);
-		if (gid != fs.gid)
-			_gid = setfsgid(gid);
-	}
-	~Cred() {
-		auto savederrno = errno;
-
-		if (_uid != NULL_UID)
-			setfsuid(_uid);
-		if (_gid != NULL_GID)
-			setfsgid(_gid);
-
-		errno = savederrno;
-	}
-};
+bool fuse_path_at::def_posixacl() const { return fs.opts.def_posixacl; }
 
 
 fuse_empty_path_at::fuse_empty_path_at(fuse_req_t req, fuse_inode &inode) :
@@ -1092,7 +1068,7 @@ static int do_getattr(const fuse_path_at &at, struct stat *attr, fuse_file_info 
 	if (fi)
 		return fstat(get_file_fd(fi), attr);
 
-	return fstatat(at.dirfd(), at.path(), attr, at.flags());
+	return at.with_cred(fstatat, at.dirfd(), at.path(), attr, at.flags());
 }
 
 static void pfs_getattr(fuse_req_t req, fuse_ino_t ino, fuse_file_info *fi)
@@ -1114,11 +1090,7 @@ static void pfs_getattr(fuse_req_t req, fuse_ino_t ino, fuse_file_info *fi)
 
 static int do_access(const fuse_path_at &at, int mask)
 {
-	auto c = fuse_req_ctx(at.req());
-	{
-		Cred cred(c->uid, c->gid);
-		return faccessat(at.dirfd(), at.path(), mask, at.flags());
-	}
+	return at.with_cred(faccessat, at.dirfd(), at.path(), mask, at.flags());
 }
 
 static void pfs_access(fuse_req_t req, fuse_ino_t ino, int mask)
@@ -1140,13 +1112,13 @@ static int do_chmod(const fuse_path_at &in, mode_t mode, fuse_file_info *fi)
 	// Convert empty path to magic symlink
 	fuse_path_at_cwd out(in);
 	// AT_SYMLINK_NOFOLLOW not implemented
-	return fchmodat(out.dirfd(), out.path(), mode, 0);
+	return out.with_cred_force(fchmodat, out.dirfd(), out.path(), mode, 0);
 }
 
 static int do_chown(const fuse_path_at &at, uid_t uid, gid_t gid, fuse_file_info *fi)
 {
 	(void)fi;
-	return fchownat(at.dirfd(), at.path(), uid, gid, at.flags());
+	return at.with_cred_force(fchownat, at.dirfd(), at.path(), uid, gid, at.flags());
 }
 
 static int do_truncate(const fuse_path_at &in, off_t size, fuse_file_info *fi)
@@ -1160,7 +1132,7 @@ static int do_truncate(const fuse_path_at &in, off_t size, fuse_file_info *fi)
 		errno = EINVAL;
 		return -1;
 	}
-	return truncate(out.path(), size);
+	return out.with_cred(truncate, out.path(), size);
 }
 
 static int do_utimens(const fuse_path_at &in, const struct timespec tv[2],
@@ -1172,7 +1144,7 @@ static int do_utimens(const fuse_path_at &in, const struct timespec tv[2],
 #ifdef HAVE_UTIMENSAT
 		// Convert empty path to magic symlink
 		fuse_path_at_cwd out(in);
-		return utimensat(out.dirfd(), out.path(), tv, out.flags());
+		return out.with_cred_force(utimensat, out.dirfd(), out.path(), tv, out.flags());
 #else
 		errno = EOPNOTSUPP;
 		return -1;
@@ -1187,18 +1159,15 @@ static void pfs_setattr(fuse_req_t req, fuse_ino_t ino, struct stat *attr,
 	if (inode.error(req))
 		return;
 
-	auto c = fuse_req_ctx(req);
 	fuse_empty_path_at at(req, inode);
 	int res;
 
 	if (valid & FUSE_SET_ATTR_MODE) {
-		Cred cred(c->uid, c->gid);
 		res = call_op(chmod)(at, attr->st_mode, fi);
 		if (res == -1)
 			goto out_err;
 	}
 	if (valid & (FUSE_SET_ATTR_UID | FUSE_SET_ATTR_GID)) {
-		Cred cred(c->uid, c->gid);
 		uid_t uid = (valid & FUSE_SET_ATTR_UID) ? attr->st_uid : NULL_UID;
 		gid_t gid = (valid & FUSE_SET_ATTR_GID) ? attr->st_gid : NULL_GID;
 
@@ -1212,7 +1181,6 @@ static void pfs_setattr(fuse_req_t req, fuse_ino_t ino, struct stat *attr,
 			goto out_err;
 	}
 	if (valid & (FUSE_SET_ATTR_ATIME | FUSE_SET_ATTR_MTIME)) {
-		Cred cred(c->uid, c->gid);
 		struct timespec tv[2];
 
 		tv[0].tv_sec = 0;
@@ -1255,8 +1223,8 @@ static int __do_lookup(const fuse_path_at &at, const char *name, fuse_entry_para
 	} else if (strcmp(name, "..") == 0) {
 		newfd = openat(dirfd, name, O_PATH | O_NOFOLLOW);
 	} else {
-		newfd = openat(dirfd, name, O_PATH | O_NOFOLLOW);
 		parent_ino = at.inode().ino();
+		newfd = at.with_cred(openat, dirfd, name, O_PATH | O_NOFOLLOW);
 	}
 	if (newfd == -1)
 		return errno;
@@ -1507,7 +1475,7 @@ static int as_user(const fuse_path_at &at, const string &opname,
 	}
 
 	{
-		Cred cred(c->uid, c->gid);
+		fuse_cred_guard cred(c->uid, c->gid, fs.uid, fs.gid);
 		auto ret = op();
 
 		if (ret == 0 || errno != EACCES)
@@ -1644,8 +1612,9 @@ static void pfs_symlink(fuse_req_t req, const char *link, fuse_ino_t parent,
 
 static int do_link(const fuse_path_at &oldat, const fuse_path_at &newat)
 {
-	return linkat(oldat.dirfd(), oldat.path(), newat.dirfd(), newat.path(),
-			oldat.flags(false));
+	return oldat.with_cred(linkat,
+			      oldat.dirfd(), oldat.path(), newat.dirfd(), newat.path(),
+			      oldat.flags(false));
 }
 
 static void pfs_link(fuse_req_t req, fuse_ino_t ino, fuse_ino_t parent,
@@ -1685,7 +1654,7 @@ static void pfs_link(fuse_req_t req, fuse_ino_t ino, fuse_ino_t parent,
 
 static int do_rmdir(const fuse_path_at &at)
 {
-	return unlinkat(at.dirfd(), at.path(), AT_REMOVEDIR);
+	return at.with_cred(unlinkat, at.dirfd(), at.path(), AT_REMOVEDIR);
 }
 
 static void pfs_rmdir(fuse_req_t req, fuse_ino_t parent, const char *name)
@@ -1707,8 +1676,8 @@ static int do_rename(const fuse_path_at &oldat, const fuse_path_at &newat,
 		return -1;
 	}
 
-	return renameat(oldat.dirfd(), oldat.path(),
-			newat.dirfd(), newat.path());
+	return oldat.with_cred(renameat, oldat.dirfd(), oldat.path(),
+			       newat.dirfd(), newat.path());
 }
 
 static void forget_one(fuse_req_t req, fuse_ino_t ino, uint64_t nlookup);
@@ -1740,7 +1709,7 @@ static void pfs_rename(fuse_req_t req, fuse_ino_t parent, const char *name,
 
 static int do_unlink(const fuse_path_at &at)
 {
-	return unlinkat(at.dirfd(), at.path(), 0);
+	return at.with_cred(unlinkat, at.dirfd(), at.path(), 0);
 }
 
 static void pfs_unlink(fuse_req_t req, fuse_ino_t parent, const char *name)
@@ -1840,7 +1809,7 @@ static void pfs_forget_multi(fuse_req_t req, size_t count,
 
 static int do_readlink(const fuse_path_at &at, char *buf, size_t size)
 {
-	return readlinkat(at.dirfd(), "", buf, size);
+	return at.with_cred(readlinkat, at.dirfd(), "", buf, size);
 }
 
 static void pfs_readlink(fuse_req_t req, fuse_ino_t ino)
@@ -1888,7 +1857,7 @@ static Dir *get_dir(fuse_file_info *fi)
 
 static int do_opendir(const fuse_path_at &at, fuse_file_info *fi)
 {
-	auto fd = openat(at.dirfd(), at.path(), fi->flags | O_DIRECTORY);
+	int fd = at.with_cred(openat, at.dirfd(), at.path(), fi->flags | O_DIRECTORY);
 	if (fd == -1)
 		return -1;
 
@@ -2191,7 +2160,7 @@ static int do_open(const fuse_path_at &in, fuse_file_info *fi)
 	auto flags = fi->flags;
 	if (out.follow())
 	       flags &= ~O_NOFOLLOW;
-	auto fd = openat(out.dirfd(), out.path(), flags);
+	int fd = out.with_cred(openat, out.dirfd(), out.path(), flags);
 	if (fd == -1)
 		return -1;
 
@@ -2471,8 +2440,8 @@ static int do_getxattr(const fuse_path_at &in, const char *name, char *value,
 		errno = EINVAL;
 		return -1;
 	}
-	return (out.follow() ? getxattr : lgetxattr)
-		(out.path(), name, value, size);
+	return out.with_cred(out.follow() ? getxattr : lgetxattr,
+			     out.path(), name, value, size);
 }
 
 static void pfs_getxattr(fuse_req_t req, fuse_ino_t ino, const char *name,
@@ -2528,7 +2497,7 @@ static int do_listxattr(const fuse_path_at &in, char *value, size_t size)
 		errno = EINVAL;
 		return -1;
 	}
-	return (out.follow() ? listxattr : llistxattr)(out.path(), value, size);
+	return out.with_cred(out.follow() ? listxattr : llistxattr, out.path(), value, size);
 }
 
 static void pfs_listxattr(fuse_req_t req, fuse_ino_t ino, size_t size)
@@ -2583,8 +2552,8 @@ static int do_setxattr(const fuse_path_at &in, const char *name, const char *val
 		errno = EINVAL;
 		return -1;
 	}
-	return (out.follow() ? setxattr : lsetxattr)
-		(out.path(), name, value, size, flags);
+	return out.with_cred_force(out.follow() ? setxattr : lsetxattr,
+				  out.path(), name, value, size, flags);
 }
 
 static void pfs_setxattr(fuse_req_t req, fuse_ino_t ino, const char *name,
@@ -2595,12 +2564,7 @@ static void pfs_setxattr(fuse_req_t req, fuse_ino_t ino, const char *name,
 		return;
 
 	fuse_empty_path_at at(req, inode);
-	auto c = fuse_req_ctx(req);
-	int res;
-	{
-		Cred cred(c->uid, c->gid);
-		res = call_op(setxattr)(at, name, value, size, flags);
-	}
+	int res = call_op(setxattr)(at, name, value, size, flags);
 	fuse_reply_errno(req, res);
 }
 
@@ -2613,7 +2577,8 @@ static int do_removexattr(const fuse_path_at &in, const char *name)
 		errno = EINVAL;
 		return -1;
 	}
-	return (out.follow() ? removexattr : lremovexattr)(out.path(), name);
+	return out.with_cred_force(out.follow() ? removexattr : lremovexattr,
+				  out.path(), name);
 }
 
 static void pfs_removexattr(fuse_req_t req, fuse_ino_t ino, const char *name)
@@ -2623,12 +2588,7 @@ static void pfs_removexattr(fuse_req_t req, fuse_ino_t ino, const char *name)
 		return;
 
 	fuse_empty_path_at at(req, inode);
-	auto c = fuse_req_ctx(req);
-	int res;
-	{
-		Cred cred(c->uid, c->gid);
-		res = call_op(removexattr)(at, name);
-	}
+	int res = call_op(removexattr)(at, name);
 	fuse_reply_errno(req, res);
 }
 #endif
